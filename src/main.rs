50cd228e925daf695b4f5a3be95e83440cd6edaa
Alexander Koch <kochalexander@gmx.net>
2019-04-18 16:07:23 +0200

    Create simple command line interface

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,50 @@ use earley::EarleyParser;
 
 use std::collections::HashSet;
 
+use docopt::Docopt;
+use serde::Deserialize;
+
+const VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
+const USAGE: &'static str = "
+thesis_bin.
+Implementation of context-free parsing algorithms.
+Copyright (c) Alexander Koch 2019
+
+Usage:
+  thesis_bin earley <grammar>
+  thesis_bin ll1 <grammar>
+  thesis_bin harrison <grammar>
+  thesis_bin (-h | --help)
+  thesis_bin --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    arg_grammar: Option<String>,
+    cmd_earley: bool,
+    cmd_ll1: bool,
+    cmd_harrison: bool,
+    flag_version: bool,
+}
+
 fn main() {
     env_logger::init();
 
-    let grammar = match ebnf::parse_grammar("harrison2.txt") {
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit());
+
+    if args.flag_version {
+        println!("thesis_bin v{}", VERSION.unwrap_or("-unknown"));
+        std::process::exit(0);
+    }
+
+    let grammar_path = args.arg_grammar.unwrap();
+    let grammar = match ebnf::parse_grammar(&grammar_path) {
         Ok(x) => Rc::new(x),
         Err(x) => { panic!("{:?}", x) }
     };
@@ -26,29 +66,30 @@ fn main() {
         println!("{}. {}", i, rule);
     }
 
-    let mut input = HashSet::new();
-    input.insert(grammar[0].head.clone());
-    //let test = harrison::predict(&grammar, &input);
-    //println!("Predict: {:?}", test);
+    let mut earley_parser = if args.cmd_earley {
+        Some(EarleyParser::new(&grammar))
+    } else {
+        None
+    };
 
-    //let mut ll = ll::FFSets::new(&grammar);
-    /*let symbols = grammar.iter().map(|x| x.head.clone()).collect::<HashSet<String>>();
-    for symbol in symbols.iter() {
-        let first = ll.first(&symbol);
-        println!("FIRST({}) = {:?}", symbol, first);
-    }
+    let mut table = if args.cmd_ll1 {
+        let mut ff = ll::FFSets::new(&grammar);
+        let symbols = grammar.iter().map(|x| x.head.clone()).collect::<HashSet<String>>();
+        for symbol in symbols.iter() {
+            let first = ff.first(&symbol);
+            println!("FIRST({}) = {:?}", symbol, first);
+        }
 
-    for symbol in symbols.iter() {
-        let follow = ll.follow(&symbol);
-        println!("FOLLOW({}) = {:?}", symbol, follow);
-    }*/
-
-    //let table = ll.construct_ll_table();
-    //println!("Table: {:?}", table);
-
-    //let accept = lr::parse_ll(&grammar, &table, vec!["a", "+", "a"]);
-
-    //let mut parser = EarleyParser::new(&grammar);
+        for symbol in symbols.iter() {
+            let follow = ff.follow(&symbol);
+            println!("FOLLOW({}) = {:?}", symbol, follow);
+        }
+        let table = ff.construct_ll_table();
+        println!("Table: {:?}", table);
+        Some(table)
+    } else {
+        None
+    };
 
     let mut rl = Editor::<()>::new();
     loop {
@@ -60,15 +101,17 @@ fn main() {
                     .collect();
 
                 println!("{:?}", words);
-                
-                /*let states = parser.analyze(&words);
-                println!("{}", earley::fmt_state_set_list(&grammar, &states));
 
-                let result = EarleyParser::accepts(&states, &words);*/
+                let result = if args.cmd_earley {
+                    let states = earley_parser.as_mut().unwrap().analyze(&words);
+                    println!("{}", earley::fmt_state_set_list(&grammar, &states));
 
-                //let result = ll::parse_ll(&grammar, &table, &words);
-
-                let result = harrison::parse(&grammar, &words);
+                    EarleyParser::accepts(&states, &words)
+                } else if args.cmd_ll1 {
+                    ll::parse_ll(&grammar, table.as_mut().unwrap(), &words)
+                } else {
+                    harrison::parse(&grammar, &words)
+                };
 
                 println!("w in L(G): {}", result);
             },
